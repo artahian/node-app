0ca34386d0cb3f7dc3d1b4fec13d1920e3f44c29
Aram <aramshat@gmail.com>
2019-07-07 22:27:30 -0700

    Add setup script

--- bin/node-app.js
+++ bin/node-app.js
@@ -9,13 +9,15 @@ const srcPath = path.join(process.cwd(), 'src');
 const distPath = path.join(process.cwd(), 'dist');
 const srcServerPath = path.join(srcPath, 'server');
 const distServerPath = path.join(distPath, 'server');
+const nodeModulesPath = path.join(process.cwd(), 'node_modules');
+const nodeModulesBinPath = path.join(nodeModulesPath, '.bin');
 
 switch (script) {
   case 'start:dev': {
     console.log('Starting dev server...');
     const ch = child_process.spawn('nodemon', [
       '--exec',
-      path.join(__dirname, '..', 'node_modules', '.bin', 'babel-node'),
+      path.join(nodeModulesBinPath, 'babel-node'),
       '--presets',
       '@babel/env,@babel/preset-react',
       path.join(srcServerPath, 'app.js')
@@ -30,9 +32,9 @@ switch (script) {
     const outputPath = path.join(distPath, 'client');
     const configPath = path.join(__dirname, '..', 'webpack.config.js');
     const cleanupCmd = `rm -rf ${distPath}`;
-    const webpackCmd = `${__dirname}/../node_modules/.bin/webpack --mode production --config='${configPath}' --output-path='${outputPath}'`;
+    const webpackCmd = `${path.join(nodeModulesBinPath, 'webpack')} --mode production --config='${configPath}' --output-path='${outputPath}'`;
 
-    const babelCmd = `${__dirname}/../node_modules/.bin/babel --presets @babel/env,@babel/preset-react ${srcServerPath} -d ${distServerPath}`;
+    const babelCmd = `${path.join(nodeModulesBinPath, 'babel')} --presets @babel/env,@babel/preset-react ${srcServerPath} -d ${distServerPath}`;
     const ch = child_process.spawn(`${cleanupCmd} && ${webpackCmd} && ${babelCmd}`, {
       shell: true
     });
@@ -44,6 +46,37 @@ switch (script) {
   case 'start': {
     console.log('Starting production server...');
     const ch = child_process.spawn(`NODE_ENV=production node ${path.join(distServerPath, 'app.js')}`, {
+      shell: true
+    });
+    ch.stdout.pipe(process.stdout);
+    ch.stderr.pipe(process.stderr);
+    break;
+  }
+
+  case 'setup': {
+    console.log('Installing dependencies...');
+    const dependencies = [
+      '@babel/cli',
+      '@babel/core',
+      '@babel/node',
+      '@babel/preset-env',
+      '@babel/preset-react',
+      'babel-loader',
+      'babel-preset-env',
+      'css-loader',
+      'html-webpack-plugin',
+      'node-sass',
+      'nodemon',
+      'sass-loader',
+      'style-loader',
+      'webpack',
+      'webpack-cli',
+      'webpack-dev-middleware',
+      'webpack-dev-server',
+      'webpack-livereload-plugin',
+    ];
+    const cmd = dependencies.map(dep => `npm install --save-dev ${dep}`).join(' && ');
+    const ch = child_process.spawn(cmd, {
       shell: true
     });
     ch.stdout.pipe(process.stdout);
